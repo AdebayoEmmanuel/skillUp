23dba340b08f437dc4605eabb2d7bc5d06b17833
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-05-10 03:08:47 +0100

    CREATE complete express app with configured api to perform CRUD operations via endpoints

---
+++ 0x00-nodejs/Users.js
@@ -0,0 +1,26 @@
+const users = [
+    {
+        id: 1,
+        name: 'John',
+        email: 'john@example.com',
+    },
+
+    {
+        id: 2,
+        name: 'Katy',
+        email: 'katy@example.com',
+    },
+
+    {
+        id: 3,
+        name: 'Faith',
+        email: 'f@example.com',
+    },
+
+    {
+        id: 4,
+        name: 'Saman',
+        email: 'saman@example.com',
+    },
+];
+module.exports = users;
