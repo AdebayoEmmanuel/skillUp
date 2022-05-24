79c067210cdfcc9c97acbf6d540f282ed35dc0b0
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-05-24 02:09:29 +0100

    CHANGE connection to 0.0.0.0:27017

--- 0x04-node_mongodb/models/db.js
+++ 0x04-node_mongodb/models/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/StudentDB', {
+mongoose.connect('mongodb://0.0.0.0:27017/StudentDB', {
     useNewUrlParser: true
 },
 err => {
     if (!err) {
+
         console.log('CONNECTION SUCCESSFUL')
     } else 
     {
