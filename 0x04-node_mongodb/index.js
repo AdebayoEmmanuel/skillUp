724e9d5203b54a3697edab27fcbe8c2080f569cd
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-05-24 02:08:00 +0100

    FIX express handlebar engine issue

--- 0x04-node_mongodb/index.js
+++ 0x04-node_mongodb/index.js
@@ -1,3 +1,5 @@
+require('./models/db');
+
 //global dependencies remember to npm --save all these dependencies
 const express = require('express');
 const path = require('path');
@@ -5,6 +7,9 @@ const handlebars = require('handlebars');
 const exphbs = require('express-handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 const bodyparser = require('body-parser');
+
+
+const studentController = require('./controllers/studentController')
 
 //create express app
 var app = express();
@@ -17,13 +22,13 @@ app.use(bodyparser.json());
 app.get('/', (req,res)=>{
     res.send(`
         <h2>Welcome to Students Database!!</h2>
-        <h3>Click here to get access to the <b> <a href="/student/list">list</a></b></h3>
+        <h3>Click here to get access to the <b> <a href="/student/list">Database</a></b></h3>
         `);
 });
 
 app.set('views', path.join(__dirname, '/views/'))
 //separating content from layout using express handlebar, similar to what xcode does with swift mobile app dev
-app.engine('hbs', exphbs({
+app.engine('hbs', exphbs.engine({
     handlebars: allowInsecurePrototypeAccess(handlebars),
     extname: 'hbs',
     defaultLayout: 'MainLayout',
@@ -37,3 +42,5 @@ app.set('view engine','hbs');
 app.listen(3000, ()=>{
     console.log('listening on port 3000');
 });
+
+app.use("/student", studentController);
