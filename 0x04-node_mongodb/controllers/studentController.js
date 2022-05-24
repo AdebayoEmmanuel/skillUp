778773d90a72177564e527d81f72c7efda6e39a8
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-05-24 02:06:58 +0100

    FIX ID cast error in stucent controller

--- 0x04-node_mongodb/controllers/studentController.js
+++ 0x04-node_mongodb/controllers/studentController.js
@@ -3,14 +3,15 @@ var router = express.Router();
 const mongoose = require('mongoose');
 const Student = mongoose.model('Student');
 
+//get method
 router.get('/', (req, res) =>{
     res.render('student/addOrEdit', {
         viewTitle: 'Insert Student'
     });
 });
-
+//POST method
 router.post('/', (req, res) =>{
-    if (req.body.id === '') {
+    if (req.body._id == '') {
         insertRecord(req, res);
     } else 
     {
@@ -31,8 +32,9 @@ function insertRecord(req, res){
             console.log('Error during insert: ' + err);
         }
     });
-}
+};
 
+//function to update student record
 updateRecord = (req, res) => {
     Student.findOneAndUpdate({_id: req.body._id}, req.body, {new: true}, (err, doc) => {
         if (!err){
@@ -40,26 +42,44 @@ updateRecord = (req, res) => {
         } else {
             console.log('Error during update: ' + err)
         }
+    });
+};
 
-    })
-}
-
+//render the record
 router.get('/list', (req, res) => {
     Student.find((err, docs) => {
         if (!err) {
             res.render('student/list', {
-                list: docs
-            })
+                list: docs,
+            });
         } else {
-            console.log('Error in retrieval: ' + err)
+            console.log('Error in retrieval: ' + err);
         }
-    })
-})
+    });
+});
 
+//get single student by id
 router.get('/:id', (req,res) => {
     Student.findById(req.params.id, (err, doc) => {
         if (!err){
-            
+            res.render('student/addOrEdit', {
+                viewTitle: 'Update Student',
+                student: doc,
+            });
+            console.log(doc);
         }
-    })
-})
+    });
+});
+
+//delete record by ID
+router.get('/delete/:id', (req, res) => {
+    Student.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err){
+            res.redirect('student/list');
+        } else {
+            console.log('Could not delete student: ' + err);
+        }
+    });
+});
+
+module.exports = router;
