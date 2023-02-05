60ac2b2b5f8c7c17c1dd5043eb2f2858a1b67203
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2023-02-05 10:43:02 +0100

    Update routes.js

--- PME_api/routes/routes.js
+++ PME_api/routes/routes.js
@@ -2,7 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 // post endpoint
--
+router.post('/post', (req, res) => {
+    res.send('PME POST Endpoint');
+});
+
+//Get all records
+router.get('/getAll', (req, res) => {
+    res.send('PME GET All Records Endpoint');
+});
+
+//Get record by ID
+router.get('/getOne/:id', (req, res) => {
+    res.send('PME GET Record by ID Endpoint');
+});
+
+//Update record by ID
+
 
 
 
